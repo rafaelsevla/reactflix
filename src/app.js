9d49c3bb6687de3c29ccf24e1a686a0cfbec0c82
Rafael Costa <rafaelsevla@gmail.com>
2019-08-25 12:56:15 -0300

    extract footer and header

--- src/app.js
+++ src/app.js
@@ -2,27 +2,26 @@
 
 import React from 'react'
 import styled, { injectGlobal } from 'styled-components'
+import Header from 'components/header'
+import Footer from 'components/footer'
 import VideosList from 'components/videos-list'
 import VideoSingle from 'components/video-single'
 import RegisterVideo from 'components/register-video'
+import { headerHeight, footerHeight } from 'utils/constants'
 
 import 'normalize.css'
 import 'milligram'
 
 const App = () => (
   <Container>
-    <Header>
-      <HeaderTitle>Reactflix</HeaderTitle>
-      <RegisterButton>Cadastrar Video</RegisterButton>
-    </Header>
-
+    <Header />
     <Main>
       <RegisterVideo />
       <VideoSingle />
       <VideosList />
     </Main>
 
-    <Footer>&copy; 2019</Footer>
+    <Footer />
   </Container>
 )
 
@@ -32,38 +31,12 @@ injectGlobal`
   }
 `
 
-const headerHeight = '60px'
-const footerHeight = '30px'
-
 const Container = styled.div`
   height: 100%;
 `
 
-const Header = styled.header`
-  align-items: center;
-  background: #333;
-  display: flex;
-  height: ${headerHeight};
-  justify-content: space-between;
-  padding: 0 10px;
-`
-
-const HeaderTitle = styled.h1`
-  color: #fff;
-  margin-bottom: 0;
-`
-
-const RegisterButton = styled.button`
-  margin-bottom: 0;
-`
-
 const Main = styled.main`
   min-height: calc(100% - ${headerHeight} - ${footerHeight});
-`
-
-const Footer = styled.footer`
-  height: ${footerHeight};
-  background: #333;
 `
 
 export default App
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+import styled from 'styled-components'
+import { footerHeight } from 'utils/constants'
+
+const MainFooter = () => <Footer>&copy; 2019</Footer>
+
+const Footer = styled.footer`
+  align-items: center;
+  background: #333;
+  color: #fff;
+  display: flex;
+  height: ${footerHeight};
+  justify-content: center;
+`
+
+export default MainFooter
